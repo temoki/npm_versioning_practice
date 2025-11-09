84ae1a0445c55a935ee42c830f6e3a90bfc513ea
Tomoki Kobayashi <temoki@gmail.com>
2025-11-09 20:59:00 +0900

    Refactor publish script to improve DEV_LATEST_VERSION determination logic and enhance readability

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,11 +11,20 @@ echo "CURRENT_VERSION=$CURRENT_VERSION"
 DEV_VERSION=$(npx semver "$CURRENT_VERSION" -i prerelease --preid="$DEV_PREID")
 echo "DEV_VERSION=$DEV_VERSION"
 
-# 
+# DEV_LATEST_VERSIONの決定
 NEXT_VERSION=$(npx semver -c "$DEV_VERSION")
-npm view "$PACKAGE@<=$DEV_VERSION < $NEXT_VERSION" version --json
-
-# 
+DEV_VERSIONS_JSON=$(npm view "$PACKAGE@<=$DEV_VERSION < $NEXT_VERSION" version --json)
+if echo "$DEV_VERSIONS_JSON" | grep -q '^\s*\['; then
+	# 配列の場合
+	DEV_LATEST_VERSION=$(echo "$DEV_VERSIONS_JSON" | node -pe 'let v=JSON.parse(require("fs").readFileSync(0,"utf8")); v[v.length-1]')
+else
+	# 文字列の場合
+	DEV_LATEST_VERSION=$(echo "$DEV_VERSIONS_JSON" | tr -d '"')
+fi
+echo "DEV_LATEST_VERSION=$DEV_LATEST_VERSION"
+DEV_VERSION=$(npx semver -i prerelease "$DEV_LATEST_VERSION" --preid="$DEV_PREID")
+echo "DEV_VERSION=$DEV_VERSION"
+ 
 # Update package.json with the new version
 # npm version "$DEV_VERSION" --no-git-tag-version
 
