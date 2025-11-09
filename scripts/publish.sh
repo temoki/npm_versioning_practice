a66d1d2b1e3b1b0596f720effe1079d2e9c56fba
Tomoki Kobayashi <temoki@gmail.com>
2025-11-09 20:52:26 +0900

    Refactor publish script to use variable for prerelease identifier and improve readability

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+PACKAGE="@temoki/npm_versioning_practice"
+DEV_PREID="dev"
+
 # Get current version from package.json
 CURRENT_VERSION=$(node -p "require('./package.json').version")
 echo "CURRENT_VERSION=$CURRENT_VERSION"
 
-# Increment version to a prerelease version with 'dev' identifier         
-DEV_VERSION=$(npx semver "$CURRENT_VERSION" -i prerelease --preid=dev)
+# Increment version to a prerelease version with 'dev' identifier
+DEV_VERSION=$(npx semver "$CURRENT_VERSION" -i prerelease --preid="$DEV_PREID")
 echo "DEV_VERSION=$DEV_VERSION"
 
+# 
+NEXT_VERSION=$(npx semver -c "$DEV_VERSION")
+npm view "$PACKAGE@<=$DEV_VERSION < $NEXT_VERSION" version --json
+
+# 
 # Update package.json with the new version
-npm version "$DEV_VERSION" --no-git-tag-version
+# npm version "$DEV_VERSION" --no-git-tag-version
 
 # Publish the package to GitHub Packages
-npm publish --tag dev --no-git-tag-version
+# npm publish --tag "$DEV_PREID" --no-git-tag-version
