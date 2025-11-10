0ddfe95433dfaa5991b10194a6a0313947bd45b3
tomoki_kobayashi <tomoki_kobayashi@mail.nissan.co.jp>
2025-11-10 18:22:12 +0900

    Add workflows for publishing dev and rc versions, and update scripts for versioning

--- scripts/publish_rc.sh
+++ scripts/publish_rc.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 PACKAGE="@temoki/npm_versioning_practice"
-PREID="dev"
+PREID="rc"
 
 echo "Get the current version from package.json (ex. 1.2.0)"
 CURRENT_VERSION=$(node -p "require('./package.json').version" | xargs npx semver)
 echo "👉 $CURRENT_VERSION"
 
-echo "Determine the next prerelease version (ex. 1.2.1-dev.0)"
-NEXT_PRE_VERSION=$(npx semver -i prerelease $CURRENT_VERSION --preid $PREID)
-echo "👉 $NEXT_PRE_VERSION"
-
-echo "Determine the next release version (ex. 1.2.1)"
-NEXT_VERSION=$(npx semver -c $NEXT_PRE_VERSION)
+echo "Determine the next release version (ex. 2.0.0)"
+NEXT_VERSION=$(npx semver -i major $CURRENT_VERSION --preid $PREID)
 echo "👉 $NEXT_VERSION"
+
+echo "Determine the next prerelease version (ex. 2.0.0-rc.0)"
+NEXT_PRE_VERSION=$(npx semver -i prerelease $NEXT_VERSION --preid $PREID)
+echo "👉 $NEXT_PRE_VERSION"
 
 echo "Check if the next prerelease version already exists in the npm registry"
 NEXT_PRE_VERSIONS=$(npm view "$PACKAGE@>=$NEXT_PRE_VERSION <$NEXT_VERSION" version --json 2>/dev/null)
 if [ $? -eq 0 ]; then
   echo "👉 $NEXT_PRE_VERSIONS"
-  echo "If the next prerelease version exists, extract the latest prerelease version (ex. 1.2.1-dev.2)"
+  echo "If the next prerelease version exists, extract the latest prerelease version (ex. 2.0.0-rc.2)"
   LATEST_NEXT_PRE_VERSION=$(echo "$NEXT_PRE_VERSIONS" | jq -r 'if type=="array" then .[-1] else . end' 2>/dev/null | xargs npx semver)
   echo "👉 $LATEST_NEXT_PRE_VERSION"
   
-  echo "Determine the next prerelease version based on the latest existing version (ex. 1.2.1-dev.3)"
+  echo "Determine the next prerelease version based on the latest existing version (ex. 2.0.0-rc.3)"
   NEXT_PRE_VERSION=$(npx semver -i prerelease $LATEST_NEXT_PRE_VERSION --preid $PREID)
 else
   echo "👉 None"
@@ -41,8 +41,8 @@ echo "* Commit: $(git rev-parse HEAD)" >> $README_FILE
 echo "👉 Done"
 
 echo "Publish the new prerelease version to npm with the preid($PREID)"
-npm version $NEXT_PRE_VERSION --no-git-tag-version
-npm publish --tag $PREID
+# npm version $NEXT_PRE_VERSION --no-git-tag-version
+# npm publish --tag $PREID
 echo "👉 Done"
 
 echo "Restore the original README file"
