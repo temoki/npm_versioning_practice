dc63b9cd2a3eeb61fc83e0b29634eda1dcbedda0
tomoki_kobayashi <tomoki_kobayashi@mail.nissan.co.jp>
2025-11-11 10:22:13 +0900

    Refactor prerelease version check to simplify error handling in npm view command

--- scripts/publish_dev.sh
+++ scripts/publish_dev.sh
@@ -18,8 +18,7 @@ NEXT_VERSION=$(npx semver -c $NEXT_PRE_VERSION)
 echo "👉 $NEXT_VERSION"
 
 echo "Check if the next prerelease version already exists in the npm registry"
-NEXT_PRE_VERSIONS=$(npm view "$PACKAGE@>=$NEXT_PRE_VERSION <$NEXT_VERSION" version --json 2>/dev/null) || true
-if [ -n "$NEXT_PRE_VERSIONS" ]; then
+if NEXT_PRE_VERSIONS=$(npm view "$PACKAGE@>=$NEXT_PRE_VERSION <$NEXT_VERSION" version --json 2>/dev/null); then
   echo "👉 $NEXT_PRE_VERSIONS"
   echo "If the next prerelease version exists, extract the latest prerelease version (ex. 1.2.1-dev.2)"
   LATEST_NEXT_PRE_VERSION=$(echo "$NEXT_PRE_VERSIONS" | jq -r 'if type=="array" then .[-1] else . end' 2>/dev/null | xargs npx semver)
